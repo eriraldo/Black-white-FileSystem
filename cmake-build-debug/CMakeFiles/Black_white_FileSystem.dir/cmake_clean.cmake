1996113a595adbe1482c267ca62a4a9e8086f6d7
Erick Elizondo <erick.elizondo20@gmail.com>
2021-06-06 17:14:22 -0600

    clion project is now configured for FUSE library

---
+++ cmake-build-debug/CMakeFiles/Black_white_FileSystem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Black_white_FileSystem"
+  "Black_white_FileSystem.pdb"
+  "CMakeFiles/Black_white_FileSystem.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Black_white_FileSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
