f6f2d01911503c183afded9a40f9f7809b25ad82
Erick Elizondo <erick.elizondo20@gmail.com>
2021-06-11 14:28:21 -0600

    testing some function

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
+/**
+ * Less Simple, Yet Stupid Filesystem.
+ *
+ * Mohammed Q. Hussain - http://www.maastaar.net
+ *
+ * This is an example of using FUSE to build a simple filesystem. It is a part of a tutorial in MQH Blog with the title "Writing Less Simple, Yet Stupid Filesystem Using FUSE in C": http://maastaar.net/fuse/linux/filesystem/c/2019/09/28/writing-less-simple-yet-stupid-filesystem-using-FUSE-in-C/
+ *
+ * License: GNU GPL
+ */
+
 #define FUSE_USE_VERSION 30
+
 #include <fuse.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -6,37 +17,100 @@
 #include <time.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+
+// ... //
+
+char dir_list[ 256 ][ 256 ];
+int curr_dir_idx = -1;
+
+char files_list[ 256 ][ 256 ];
+int curr_file_idx = -1;
+
+char files_content[ 256 ][ 256 ];
+int curr_file_content_idx = -1;
+
+void add_dir( const char *dir_name )
+{
+    curr_dir_idx++;
+    strcpy( dir_list[ curr_dir_idx ], dir_name );
+}
+
+int is_dir( const char *path )
+{
+    path++; // Eliminating "/" in the path
+
+    for ( int curr_idx = 0; curr_idx <= curr_dir_idx; curr_idx++ )
+        if ( strcmp( path, dir_list[ curr_idx ] ) == 0 )
+            return 1;
+
+    return 0;
+}
+
+void add_file( const char *filename )
+{
+    curr_file_idx++;
+    strcpy( files_list[ curr_file_idx ], filename );
+
+    curr_file_content_idx++;
+    strcpy( files_content[ curr_file_content_idx ], "" );
+}
+
+int is_file( const char *path )
+{
+    path++; // Eliminating "/" in the path
+
+    for ( int curr_idx = 0; curr_idx <= curr_file_idx; curr_idx++ )
+        if ( strcmp( path, files_list[ curr_idx ] ) == 0 )
+            return 1;
+
+    return 0;
+}
+
+int get_file_index( const char *path )
+{
+    path++; // Eliminating "/" in the path
+
+    for ( int curr_idx = 0; curr_idx <= curr_file_idx; curr_idx++ )
+        if ( strcmp( path, files_list[ curr_idx ] ) == 0 )
+            return curr_idx;
+
+    return -1;
+}
+
+void write_to_file( const char *path, const char *new_content )
+{
+    int file_idx = get_file_index( path );
+
+    if ( file_idx == -1 ) // No such file
+        return;
+
+    strcpy( files_content[ file_idx ], new_content );
+}
+
+// ... //
 
 static int do_getattr( const char *path, struct stat *st )
 {
-    printf( "[getattr] Called\n" );
-    printf( "\tAttributes of %s requested\n", path );
-
-    // GNU's definitions of the attributes (http://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html):
-    // 		st_uid: 	The user ID of the file’s owner.
-    //		st_gid: 	The group ID of the file.
-    //		st_atime: 	This is the last access time for the file.
-    //		st_mtime: 	This is the time of the last modification to the contents of the file.
-    //		st_mode: 	Specifies the mode of the file. This includes file type information (see Testing File Type) and the file permission bits (see Permission Bits).
-    //		st_nlink: 	The number of hard links to the file. This count keeps track of how many directories have entries for this file. If the count is ever decremented to zero, then the file itself is discarded as soon
-    //						as no process still holds it open. Symbolic links are not counted in the total.
-    //		st_size:	This specifies the size of a regular file in bytes. For files that are really devices this field isn’t usually meaningful. For symbolic links this specifies the length of the file name the link refers to.
-
     st->st_uid = getuid(); // The owner of the file/directory is the user who mounted the filesystem
     st->st_gid = getgid(); // The group of the file/directory is the same as the group of the user who mounted the filesystem
     st->st_atime = time( NULL ); // The last "a"ccess of the file/directory is right now
     st->st_mtime = time( NULL ); // The last "m"odification of the file/directory is right now
 
-    if ( strcmp( path, "/" ) == 0 )
+    if ( strcmp( path, "/" ) == 0 || is_dir( path ) == 1 )
     {
         st->st_mode = S_IFDIR | 0755;
         st->st_nlink = 2; // Why "two" hardlinks instead of "one"? The answer is here: http://unix.stackexchange.com/a/101536
     }
-    else
+    else if ( is_file( path ) == 1 )
     {
         st->st_mode = S_IFREG | 0644;
         st->st_nlink = 1;
         st->st_size = 1024;
+    }
+    else
+    {
+        return -ENOENT;
     }
 
     return 0;
@@ -44,15 +118,16 @@ static int do_getattr( const char *path, struct stat *st )
 
 static int do_readdir( const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi )
 {
-    printf( "--> Getting The List of Files of %s\n", path );
-
     filler( buffer, ".", NULL, 0 ); // Current Directory
     filler( buffer, "..", NULL, 0 ); // Parent Directory
 
     if ( strcmp( path, "/" ) == 0 ) // If the user is trying to show the files/directories of the root directory show the following
     {
-        filler( buffer, "file54", NULL, 0 );
-        filler( buffer, "file349", NULL, 0 );
+        for ( int curr_idx = 0; curr_idx <= curr_dir_idx; curr_idx++ )
+            filler( buffer, dir_list[ curr_idx ], NULL, 0 );
+
+        for ( int curr_idx = 0; curr_idx <= curr_file_idx; curr_idx++ )
+            filler( buffer, files_list[ curr_idx ], NULL, 0 );
     }
 
     return 0;
@@ -60,36 +135,50 @@ static int do_readdir( const char *path, void *buffer, fuse_fill_dir_t filler, o
 
 static int do_read( const char *path, char *buffer, size_t size, off_t offset, struct fuse_file_info *fi )
 {
-    printf( "--> Trying to read %s, %ld, %ld\n", path, offset, size );
+    int file_idx = get_file_index( path );
 
-    char file54Text[] = "Hello World From File54!";
-    char file349Text[] = "Hello World From File349!";
-    char *selectedText = NULL;
-
-    // ... //
-
-    if ( strcmp( path, "/file54" ) == 0 )
-        selectedText = file54Text;
-    else if ( strcmp( path, "/file349" ) == 0 )
-        selectedText = file349Text;
-    else
+    if ( file_idx == -1 )
         return -1;
+    char *content = files_content[ file_idx ];
 
-    // ... //
+    memcpy( buffer, content + offset, size );
 
-    memcpy( buffer, selectedText + offset, size );
+    return strlen( content ) - offset;
+}
 
-    return strlen( selectedText ) - offset;
+static int do_mkdir( const char *path, mode_t mode )
+{
+    path++;
+    add_dir( path );
+
+    return 0;
+}
+
+static int do_mknod( const char *path, mode_t mode, dev_t rdev )
+{
+    path++;
+    add_file( path );
+
+    return 0;
+}
+
+static int do_write( const char *path, const char *buffer, size_t size, off_t offset, struct fuse_file_info *info )
+{
+    write_to_file( path, buffer );
+
+    return size;
 }
 
 static struct fuse_operations operations = {
         .getattr	= do_getattr,
         .readdir	= do_readdir,
         .read		= do_read,
+        .mkdir		= do_mkdir,
+        .mknod		= do_mknod,
+        .write		= do_write,
 };
 
 int main( int argc, char *argv[] )
 {
-    printf("test2");
     return fuse_main( argc, argv, &operations, NULL );
 }
